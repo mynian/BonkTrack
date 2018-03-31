ed0a5a0ec57647deaa9b119685b932d971521f9e
mynian <mynian@gmail.com>
2018-03-30 20:17:08 -0400

    Update BonkTrack.lua

--- BonkTrack.lua
+++ BonkTrack.lua
@@ -1,1 +1,29 @@
+--Create the table
+bonktrack = {}
 
+local emotetype, btname
+
+local function btracker(token)
+	emotetype = token
+	--print(emotetype)
+	btname = GetUnitName("target", true)
+	tartype = UnitIsPlayer("target")
+	--print(tartype)
+	--print(btname)
+	if tartype == true then
+		if btname ~= nil then
+			if emotetype == "BONK" then
+				--print("Success")
+				bonktrack[btname] = bonktrack[btname] and bonktrack[btname]+1 or 1
+				print("BONKed " .. btname .. " " .. bonktrack[btname] .. " times")
+			else
+				print("BONK fail")
+			end
+		else
+			print("btname fail")
+		end
+	else
+		print("Not a player or no target")
+	end
+end
+hooksecurefunc("DoEmote", btracker)
